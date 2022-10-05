fda0c3d227e3fd2ed20edeb59be94f88eff64765
DiogoB0mfim <diogohbomfim@gmail.com>
2022-10-05 14:48:51 -0300

    update: arrumei as pastas do database

--- src/models/Teacher.ts
+++ src/models/Teacher.ts
@@ -1,27 +1,20 @@
 import { Person } from "./Person";
 
 export class Teacher extends Person {
-    constructor(
-        id : string,
-        name : string,
-        email : string,
-        dataNasc : string,
-        classId : string,
-        private specialty : string 
-       ){
-        super(
-            id,
-            name,
-            email,
-            dataNasc,
-            classId
-        )
-        
-        this.specialty = specialty
+  constructor(
+    id: string,
+    name: string,
+    email: string,
+    dataNasc: string,
+    classId: string,
+    private specialty: string
+  ) {
+    super(id, name, email, dataNasc, classId);
 
-       } 
+    this.specialty = specialty;
+  }
 
-       public getSpecialty() {
-            return this.specialty
-       }
+  public getSpecialty() {
+    return this.specialty;
+  }
 }
